4e044a6eef52f706f38cf4b9028db94a77a56cb6
mardoqueu <mdq767@gmail.com>
2022-08-23 09:26:17 -0300

    create footer component

--- .history/pages/index_20220823091931.js
+++ .history/pages/index_20220823091931.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Header from '../components/Header'
-import Footer from "../components/Footer";
 import Image from "next/image";
 import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
 
--- .history/pages/index_20220823091944.js
+++ .history/pages/index_20220823091944.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
-import Footer from "../components/Footer";
+import Header from '../components/Footer'
 import Image from "next/image";
 import { SearchIcon, MicrophoneIcon } from "@heroicons/react/solid";
 
