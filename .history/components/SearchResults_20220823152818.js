674bbf872b999ce7c0bb93eaec09f1bb6e329878
mardoqueu <mdq767@gmail.com>
2022-08-23 15:37:00 -0300

    complete the result section and isntall html react parser

--- .history/components/SearchResults_20220823152818.js
+++ .history/components/SearchResults_20220823152818.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Parser from "html-react-parser"
 
 export default function SearchResults({results}){
     return(
--- .history/components/SearchResults_20220823153104.js
+++ .history/components/SearchResults_20220823153104.js
@@ -14,6 +14,7 @@ export default function SearchResults({results}){
                         </a>
                     </div>
                     <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
+
             </div>
         ))}
        
